0e2f6f236e6268da079ee7a2cd24a3c8ded9b243
huang <3042569263@qq.com>
2022-05-28 09:38:27 +0800

    文档

---
+++ standard_2022/编程指南/CSDN及其他/CSDN_文档中心_编程修养（二）.files/navigation.js
@@ -0,0 +1,30 @@
+document.write("<table width='100%' border='0' cellspacing='0' height='20' cellpadding='0'>");
+document.write("  <tr>");
+document.write("	<td>");
+document.write("<font color='#ffffff'>");
+document.write("	 <a href='http://www.csdn.net/' target='_blank'><font color='#ffffff'>首&nbsp;页</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/news/' target='_blank'><font color='#ffffff'>新&nbsp;闻</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/develop/' target='_blank'><font color='#ffffff'>技&nbsp;术</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/expert/' target='_blank'><font color='#ffffff'>论&nbsp;坛</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/commerce/#job' target='_blank'><font color='#ffffff'>人&nbsp;才</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/commerce/#buy' target='_blank'><font color='#ffffff'>商&nbsp;城</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/commerce/#educate' target='_blank'><font color='#ffffff'>培&nbsp;训</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/magazine/' target='_blank'><font color='#ffffff'>《程序员》</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/cnshare/' target='_blank'><font color='#ffffff'>软&nbsp;件</font></a>");
+
+document.write("&nbsp;&nbsp;|&nbsp;&nbsp;<a href='http://www.csdn.net/ibmc/' target='_blank'><font color='#ffffff'>IBM专区</font></a>");
+
+document.write("  </font>");
+document.write("  </td></tr>");
+document.write("</table>");
+
+
+
---
+++ standard_2022/编程指南/CSDN及其他/CSDN_编程修养（四）.files/link.js
@@ -0,0 +1,3 @@
+document.write("<a href=http://www.csdn.net/buy/product/product.asp?id=1292 target=_blank><img src=http://www.csdn.net/images/ad/vsnet_120.gif width=120 height=60 border=0></a>")
+
+
